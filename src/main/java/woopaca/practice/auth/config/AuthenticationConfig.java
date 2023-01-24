aea0d767fa1535163b820e60412966a25cfdaa05
woopaca <jcw001031@gmail.com>
2023-01-24 11:03:26 +0900

    접근 권한 변경

--- src/main/java/woopaca/practice/auth/config/AuthenticationConfig.java
+++ src/main/java/woopaca/practice/auth/config/AuthenticationConfig.java
@@ -2,22 +2,25 @@ package woopaca.practice.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class AuthenticationConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .httpBasic().disable() // UI 쪽 disable
+                .httpBasic().disable() // UI 인증이 아닌 토큰 인이기 때문에 basic disable
                 .csrf().disable() // cross site 기능?
                 .cors().and() //cross site에서 도메인이 다를 때 허용
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll() // 허용
                 .antMatchers("/api/v1/users/sign-up", "/api/v1/users/sign-in").permitAll() // 허용
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt 이용 시 사용
